97b70f2805c8c3475938d8a6c77125d301a2fb53
Ameya Vichare <ameyavi@gmail.com>
2021-03-22 15:27:00 +0530

    Initial Commit

---
+++ SportzInteractiveApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportzInteractiveApp
+//
+//  Created by Ameya Vichare on 22/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
